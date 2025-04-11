1c6d3d8218db97451813d8575545089348ec11fb
Samraysh Pellakur <pellakur.samraysh@gmail.com>
2025-04-11 02:52:11 -0400

    Added the base files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+    showPopup();
+  };
+  
+  const overlay = document.getElementById("overlay");
+  const continueBtn = document.getElementById("continueBtn");
+  const reopenPopupBtn = document.getElementById("reopenPopupBtn");
+  
+  function showPopup() {
+    overlay.classList.remove("hidden");
+  }
+  
+  function hidePopup() {
+    overlay.classList.add("hidden");
+  }
+  
+  continueBtn.addEventListener("click", hidePopup);
+  reopenPopupBtn.addEventListener("click", showPopup);
